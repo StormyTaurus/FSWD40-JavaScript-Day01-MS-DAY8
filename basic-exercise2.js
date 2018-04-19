421b41cb34fe45d227cc4e0052d5c7b1f45fcf1c
Mario <marioschantel@gmail.com>
2018-04-19 10:28:04 +0200

    basic all

---
+++ basic-exercise2.js
@@ -0,0 +1,14 @@
+function myFunction() {
+
+
+document.getElementById ("basic2").innerHTML = "Hi my name is Jeremy";
+
+
+
+}
+
+
+function showMessage() {
+document.write('Hi my name is Jeremy');
+}
+showMessage();
